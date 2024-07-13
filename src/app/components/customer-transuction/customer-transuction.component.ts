d245a842f4a82cb2645ee5e71d05fe307b298236
unknown <ramyihabb0418@gmail.com>
2024-07-13 13:46:27 +0300

    make data-service , customer-html and TS

--- src/app/components/customer-transuction/customer-transuction.component.ts
+++ src/app/components/customer-transuction/customer-transuction.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { transition } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
 
 @Component({
   selector: 'app-customer-transuction',
   templateUrl: './customer-transuction.component.html',
   styleUrls: ['./customer-transuction.component.scss']
 })
-export class CustomerTransuctionComponent {
+export class CustomerTransuctionComponent implements OnInit {
+  constructor(private _DataService: DataService) { }
+
+  customers: any[] = [];
+  transactions: any[] = [];
+  filteredTransactions: any[] = [];
+  selectedCustomer: any = null;
+
+  ngOnInit(): void {
+
+    this._DataService.getCustomers().subscribe((data: any) => {
+      this.customers = data;
+    })
+
+    this._DataService.getTransactions().subscribe((data: any) => {
+      this.transactions = data;
+    })
+  };
+
+  // filterTransactions(customerId: number): void {
+  //   if (customerId) {
+  //     this.filteredTransactions = this.transactions.filter(
+  //       (transaction) => transaction.customer_id === customerId
+  //     );
+  //   } else {
+  //     this.filteredTransactions = this.transactions;
+  //   }
+  // }
+
+  // selectCustomer(customer: any): void {
+  //   this.selectedCustomer = customer;
+  //   this.filterTransactions(customer.id);
+  // }
+
+
+  filterTransaction(customerId: number): void {
+    if (customerId) {
+      this.filteredTransactions = this.transactions.filter(
+        (transaction) => transaction.customer_id === customerId
+      );
+    } else {
+      this.filteredTransactions = this.transactions;
+    }
+
+  }
+
+  selectCustomer(customer: any): void {
+    this.selectedCustomer = customer;
+    this.filterTransaction(customer.id);
+
+
+  }
+
 
 }
+
+
+
